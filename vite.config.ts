0142f5c00e231e0db02cffac871fd910e6ea6235
Pradeepkh312 <pradeepkh312@gmail.com>
2026-01-03 14:22:09 +0530

    base name removed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/pings-lab.github.io/',
+  base: '/',
   server: {
     host: "0.0.0.0",
     port: 8000,
